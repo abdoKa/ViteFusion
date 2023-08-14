626c3546a9df5f69a617ef5290d8bd7da441b9f8
Abdelali Kabou <34213161+abdoKa@users.noreply.github.com>
2023-08-14 19:36:03 +0100

    feat: init a default component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { Box, Center, Text } from "@chakra-ui/react";
 import { useTranslation } from "react-i18next";
+import SayHello from "./components/SayHello";
 
 function App() {
   const { t } = useTranslation();
   return (
     <Box>
       <Center h="100vh" flexDirection="column">
-        <Text mb={4}>{t("startCoding")}</Text>
+        <SayHello greeting={`${t("startCoding")}`}  />
         <Text fontSize="sm" color="gray.600">Developed by Abdelali Kabou</Text>
       </Center>
     </Box>
---
+++ src/components/SayHello.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text } from '@chakra-ui/react'
+
+type Props = {
+  greeting: string
+}
+
+export default function SayHello({greeting}: Props) {
+  return (
+    <Text>{greeting}</Text>
+  )
+}
